8e42e110597dd2fd419184aa245c606121be1f57
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-29 20:06:27 -0300

    Primitivos numericos de punto flotante y sus constantes

--- HolaMundo/src/PrimitivosFloat.java
+++ HolaMundo/src/PrimitivosFloat.java
@@ -3,5 +3,19 @@ public class PrimitivosFloat {
 
         float realFloat = 2.12e3f; //2120f;
         System.out.println("realFloat = " + realFloat);
+        System.out.println("float corresponde en byte a = " + Float.BYTES);
+        System.out.println("float corresponde en bites a = " + Float.SIZE);
+        System.out.println("máximo valor para float = " + Float.MAX_VALUE);
+        System.out.println("minimo valor para float = " + Float.MIN_VALUE);
+
+        double realDouble = 3.4028235E38;
+        System.out.println("realDouble = " + realDouble);
+        System.out.println("double corresponde en byte a = " + Double.BYTES);
+        System.out.println("double corresponde en bites a = " + Double.SIZE);
+        System.out.println("máximo valor para double = " + Double.MAX_VALUE);
+        System.out.println("minimo valor para double = " + Double.MIN_VALUE);
+
+        var varFloate = 3.1416;
+        System.out.println("varFloate = " + varFloate);
     }
 }
