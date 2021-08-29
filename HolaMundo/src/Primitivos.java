91098c9307aaa1b95d50d46d50751b80093fb284
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-29 18:24:56 -0300

    Primitivos numericos enteros

---
+++ HolaMundo/src/Primitivos.java
@@ -0,0 +1,34 @@
+public class Primitivos {
+    public static void main(String[] args) {
+
+        byte numeroByte = 127;
+        System.out.println("numeroByte = " + numeroByte);
+        System.out.println("Tipo byte corresponde en byte a " + Byte.BYTES);
+        System.out.println("Tipo byte corresponde en bites a " + Byte.SIZE);
+        System.out.println("Valor máximo de de un byte: " + Byte.MAX_VALUE);
+        System.out.println("Valor máximo de de un byte: " + Byte.MIN_VALUE);
+
+        short numeroShort = 32767;
+        System.out.println("numeroShort = " + numeroShort);
+        System.out.println("Tipo short corresponde en byte a " + Short.BYTES);
+        System.out.println("Tipo short corresponde en bites a " + Short.SIZE);
+        System.out.println("Valor máximo de de un short: " + Short.MAX_VALUE);
+        System.out.println("Valor máximo de de un short: " + Short.MIN_VALUE);
+
+        int numeroInt = 2147483647;
+        System.out.println("numeroInt = " + numeroInt);
+        System.out.println("Tipo int corresponde en byte a " + Integer.BYTES);
+        System.out.println("Tipo int corresponde en bites a " + Integer.SIZE);
+        System.out.println("Valor máximo de de un int: " + Integer.MAX_VALUE);
+        System.out.println("Valor máximo de de un int: " + Integer.MIN_VALUE);
+
+        long numeroLong = 9223372036854775807L;
+        System.out.println("numeroLong = " + numeroLong);
+        System.out.println("Tipo long corresponde en byte a " + Long.BYTES);
+        System.out.println("Tipo long corresponde en bites a " + Long.SIZE);
+        System.out.println("Valor máximo de de un long: " + Long.MAX_VALUE);
+        System.out.println("Valor máximo de de un long: " + Long.MIN_VALUE);
+
+        var numeroVar = 127;
+    }
+}
