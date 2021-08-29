00072aa1c191567c79602fe8f2ef693ebd707a34
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-29 19:27:31 -0300

    Primitivos numericos de punto flotante

---
+++ HolaMundo/src/PrimitivosFloat.java
@@ -0,0 +1,7 @@
+public class PrimitivosFloat {
+    public static void main(String[] args) {
+
+        float realFloat = 2.12e3f; //2120f;
+        System.out.println("realFloat = " + realFloat);
+    }
+}
