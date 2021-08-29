6066e19a19b541b56e1bafe12cbe4a99c9be9a6f
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-29 15:52:22 -0300

    Tarea: Imprime tu nombre en consola

---
+++ MiNombre/src/MiNombre.java
@@ -0,0 +1,6 @@
+public class MiNombre {
+    public static void main(String[] args) {
+        String nombre = "Eraldo Vicente de Brito Júnior";
+        System.out.println("Hola mi nombre es " + nombre);
+    }
+}
