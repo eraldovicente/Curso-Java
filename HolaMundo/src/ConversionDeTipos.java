1b36f20036145a1d08c3ea6378437fed4296377a
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-30 21:33:58 -0300

    Conversion de cadenas a primitivos

---
+++ HolaMundo/src/ConversionDeTipos.java
@@ -0,0 +1,16 @@
+public class ConversionDeTipos {
+    public static void main(String[] args) {
+        String numeroStr = "50";
+
+        int numeroInt = Integer.parseInt(numeroStr);
+        System.out.println("numeroInt = " + numeroInt);
+
+        String realStr = "98765.43e-3";
+        double realDouble = Double.parseDouble(realStr);
+        System.out.println("realDouble = " + realDouble);
+
+        String logicoStr = "true";
+        boolean logicoBoolean = Boolean.parseBoolean(logicoStr);
+        System.out.println("logicoBoolean = " + logicoBoolean);
+    }
+}
