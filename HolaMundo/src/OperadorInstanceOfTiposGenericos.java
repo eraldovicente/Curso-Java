9757c15807e50eda42e7d7e9321c1e4e9c1d928b
Eraldo Vicente <eraldovicente2@gmail.com>
2021-09-01 20:16:27 -0300

    Operador Instanceof con tipos abstratos

--- HolaMundo/src/OperadorInstanceOfTiposGenericos.java
+++ HolaMundo/src/OperadorInstanceOfTiposGenericos.java
@@ -1,18 +1,15 @@
-public class OperadorInstanceOf {
+public class OperadorInstanceOfTiposGenericos {
     public static void main(String[] args) {
 
-        Object texto = "Creando un objeto de la clase String .. que tal!";
+        String texto = "Creando un objeto de la clase String .. que tal!";
 
-        Number num = Integer.valueOf(7); // 7;
+        Integer num = 7;
 
         Boolean b1 = texto instanceof String;
         System.out.println("texto es del tipo String = " + b1);
 
         b1 = texto instanceof Object;
         System.out.println("texto es del tipo Object = " + b1);
-
-        b1 = texto instanceof Integer;
-        System.out.println("texto es del tipo Integer = " + b1);
 
         b1 = num instanceof Integer;
         System.out.println("num es del tipo Integer = " + num);
@@ -23,16 +20,7 @@ public class OperadorInstanceOf {
         b1 = num instanceof Object;
         System.out.println("num es del tipo Object = " + num);
 
-        b1 = num instanceof Long;
-        System.out.println("num es del tipo Long = " + b1);
-
-        b1 = num instanceof Double;
-        System.out.println("num es del tipo Double = " + b1);
-
-        Number decimal = 45.54;
-        b1 = decimal instanceof Double;
-        System.out.println("decimal es del tipo Double = " + decimal);
-
+        Double decimal = 45.54;
         b1 = decimal instanceof Number;
         System.out.println("decimal es del tipo Number = " + decimal);
 
