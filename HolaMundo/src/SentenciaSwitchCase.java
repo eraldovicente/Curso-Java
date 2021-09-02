0074aa1b05771f6d6bf41ac7c95d5fd7885efe74
Eraldo Vicente <eraldovicente2@gmail.com>
2021-09-02 20:39:42 -0300

    Sentencia switch case ejemplo nombre del mes

--- HolaMundo/src/SentenciaSwitchCase.java
+++ HolaMundo/src/SentenciaSwitchCase.java
@@ -1,5 +1,56 @@
+import java.util.Scanner;
+
 public class SentenciaSwitchCase {
     public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Ingrese el número del mes entre 1 - 12:");
+        int mes = s.nextInt();
+        String nombreMes = null;
+
+        switch(mes) {
+            case 1:
+                nombreMes = "Enero";
+                break;
+            case 2:
+                nombreMes = "Febrero";
+                break;
+            case 3:
+                nombreMes = "Marzo";
+                break;
+            case 4:
+                nombreMes = "Abril";
+                break;
+            case 5:
+                nombreMes = "Mayo";
+                break;
+            case 6:
+                nombreMes = "Junio";
+                break;
+            case 7:
+                nombreMes = "Julio";
+                break;
+            case 8:
+                nombreMes = "Agosto";
+                break;
+            case 9:
+                nombreMes = "Septiembro";
+                break;
+            case 10:
+                nombreMes = "Octubre";
+                break;
+            case 11:
+                nombreMes = "Noviembre";
+                break;
+            case 12:
+                nombreMes = "Diciembre";
+                break;
+            default:
+                nombreMes = "Indefinido";
+        }
+
+        System.out.println("nombreMes = " + nombreMes);
+
         byte num = 1;
 
         switch(num){
