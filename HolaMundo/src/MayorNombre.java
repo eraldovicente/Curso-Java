17e0a288396de7a477d964a79c99a8795c32beb5
Eraldo Vicente <eraldovicente2@gmail.com>
2021-09-01 22:44:09 -0300

    Tarea 6

---
+++ HolaMundo/src/MayorNombre.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class MayorNombre {
+    public static void main(String[] args) {
+        String nombre1 = JOptionPane.showInputDialog("Digite nombre completo (nombre + apellido) del familiar");
+        String nombre2 = JOptionPane.showInputDialog("Digite nombre completo (nombre + apellido) del segundo familiar");
+        String nombre3 = JOptionPane.showInputDialog("Digite nombre completo (nombre + apellido) del tercer familiar");
+
+        String[] split1 = nombre1.split(" ");
+        String[] split2 = nombre2.split(" ");
+        String[] split3 = nombre3.split( " ");
+
+        if (split1.length > split2.length && split1.length > split3.length) {
+            System.out.println(nombre1 + " tiene el nombre más largo.");
+        } else if (split2.length > split1.length && split2.length > split3.length) {
+            System.out.println(nombre2 + " tiene el nombre más largo.");
+        } else {
+            System.out.println(nombre3 + " tiene el nombre más largo.");
+        }
+    }
+}
+
+/*
+* ze
+* jose
+* migue
+* */
