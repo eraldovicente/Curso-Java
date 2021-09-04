fa7ec68fdbe1a6ff2f1468c7fb1a37cdf35e7c9a
Eraldo Vicente <eraldovicente2@gmail.com>
2021-09-04 18:01:27 -0300

    Convertir una fecha String a objeto del tipo Date

---
+++ HolaMundo/src/EjemploJavaUtilDateParse.java
@@ -0,0 +1,21 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class EjemploJavaUtilDateParse {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+        System.out.println("Ingrese una fecha con formato 'yyyy-MM-dd");
+        try {
+            Date fecha = format.parse(s.next());
+            System.out.println("fecha = " + fecha);
+            System.out.println("format = " + format.format(fecha));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
