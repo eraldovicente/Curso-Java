11c682fd4c4a77aa7662cf8e3248b3ce44bb93cc
Eraldo Vicente <eraldovicente2@gmail.com>
2021-09-04 23:34:08 -0300

    La clase Math parte 2 (trigonometria)

--- HolaMundo/src/EjemploClaseMath.java
+++ HolaMundo/src/EjemploClaseMath.java
@@ -21,5 +21,29 @@ public class EjemploClaseMath {
 
         long entero = Math.round(Math.PI);
         System.out.println("entero = " + entero);
+
+        double exp = Math.exp(2);
+        System.out.println("exp = " + exp);
+
+        double log = Math.log(10);
+
+        double potencia = Math.pow(10, 3);
+        System.out.println("potencia = " + potencia);
+
+        double raiz = Math.sqrt(5);
+        System.out.println("raiz = " + raiz);
+
+        double grados = Math.toDegrees(1.57);
+        grados = Math.round(grados);
+        System.out.println("Convertir de radianos a grados " + grados);
+
+        double radianes = Math.toRadians(90.00);
+        System.out.println("Convertir de grados a radianes " + radianes);
+
+        System.out.println("sin(90): " + Math.sin(radianes));
+        System.out.println("cos(90): " + Math.cos(radianes));
+
+        radianes = Math.toRadians(180.00);
+        System.out.println("cos(180): " + Math.cos(radianes));
     }
 }
