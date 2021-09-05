de11b9703fdd15eb0b9765baa983eb3c39ca7ecc
Eraldo Vicente <eraldovicente2@gmail.com>
2021-09-04 23:18:37 -0300

    La clase Math parte 1

---
+++ HolaMundo/src/EjemploClaseMath.java
@@ -0,0 +1,25 @@
+public class EjemploClaseMath {
+    public static void main(String[] args) {
+
+        int absoluto = Math.abs(-3);
+        System.out.println("absoluto = " + absoluto);
+
+        absoluto = Math.abs(3);
+        System.out.println("absoluto = " + absoluto);
+
+        double max = Math.max(3.5, 1.2);
+        System.out.println("max = " + max);
+
+        double min = Math.min(3.5, 1.2);
+        System.out.println("min = " + min);
+
+        double techo = Math.ceil(3.5);
+        System.out.println("techo = " + techo);
+
+        double piso = Math.floor(3.5);
+        System.out.println("piso = " + piso);
+
+        long entero = Math.round(Math.PI);
+        System.out.println("entero = " + entero);
+    }
+}
