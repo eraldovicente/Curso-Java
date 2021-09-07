477eaa9bd7dbebbc83ceb64dd058b8af7bb1ab28
Eraldo Vicente <eraldovicente2@gmail.com>
2021-09-07 01:27:39 -0300

    Combinar dos arreglos en un entero

---
+++ arreglos/src/EjemploArreglosCombinados.java
@@ -0,0 +1,28 @@
+public class EjemploArreglosCombinados {
+    public static void main(String[] args) {
+
+        int[] a, b, c;
+        a = new int[10];
+        b = new int[10];
+        c = new int[20];
+
+        for(int i = 0; i < a.length; i++) {
+            a[i] = i + 1;
+        }
+
+        for(int i = 0; i < b.length; i++) {
+            b[i] = (i + 1) * 5;
+        }
+
+        int aux = 0;
+        for(int i = 0; i < 10; i++) {
+            c[aux++] = a[i];
+            c[aux++] = b[i];
+        }
+
+        for(int i = 0; i < c.length; i++) {
+            System.out.println(i + " : " + c[i]);
+        }
+
+    }
+}
