316d09b76659d466f47da9a3937fe1c327c36b7e
Eraldo Vicente <eraldovicente2@gmail.com>
2021-09-06 21:27:46 -0300

    Ordenamiento algoritmo burbuja metodo optimizado

--- arreglos/src/EjemploArregloForOndenamientoBurbuja.java
+++ arreglos/src/EjemploArregloForOndenamientoBurbuja.java
@@ -15,12 +15,12 @@ public class EjemploArregloForOndenamientoBurbuja {
 
         int contador = 0;
 
-        for(int i = 0; i < total; i++) {
-            for(int j = 0; j < total; j++) {
-                if(productos[i].compareTo(productos[j]) < 0) {
-                    String auxiliar = productos[i];
-                    productos[i] = productos[j];
-                    productos[j] = auxiliar;
+        for(int i = 0; i < total - 1; i++) {
+            for(int j = 0; j < total - 1 - i; j++) {
+                if(productos[j+1].compareTo(productos[j]) < 0) {
+                    String auxiliar = productos[j];
+                    productos[j] = productos[j+1];
+                    productos[j+1] = auxiliar;
                 }
                 contador++;
             }
