54142fdf58c1740719273e8268e7c0099db280c4
Eraldo Vicente <eraldovicente2@gmail.com>
2021-09-09 18:09:11 -0300

    Creando nuestra primera clase

---
+++ poo/src/Automovil.java
@@ -0,0 +1,8 @@
+public class Automovil {
+
+    String fabricante;
+    String modelo;
+    String color;
+    double cilindrada;
+
+}
---
+++ poo/src/EjemploAutomovil.java
@@ -0,0 +1,5 @@
+public class EjemploAutomovil {
+    public static void main(String[] args) {
+
+    }
+}
