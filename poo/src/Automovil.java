68a285a51065eadecfad175807e920268768a70e
Eraldo Vicente <eraldovicente2@gmail.com>
2021-09-09 21:07:17 -0300

    Agregando metodos

--- poo/src/Automovil.java
+++ poo/src/Automovil.java
@@ -5,4 +5,13 @@ public class Automovil {
     String color = "gris";
     double cilindrada;
 
+    public String detalle() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("auto.fabricante = " + this.fabricante);
+        sb.append("\nauto.modelo = " + this.modelo);
+        sb.append("\nauto.color = " + this.color);
+        sb.append("\nauto.cilindrada = " + this.cilindrada);
+        return sb.toString();
+    }
+
 }
--- poo/src/EjemploAutomovil.java
+++ poo/src/EjemploAutomovil.java
@@ -1,12 +1,11 @@
 public class EjemploAutomovil {
     public static void main(String[] args) {
 
-        Automovil auto = new Automovil();
-
-        auto.fabricante = "Subaru";
-        auto.modelo = "Impreza";
-        auto.cilindrada = 2.0;
-        auto.color = "Blanco";
+        Automovil subaru = new Automovil();
+        subaru.fabricante = "Subaru";
+        subaru.modelo = "Impreza";
+        subaru.cilindrada = 2.0;
+        subaru.color = "Blanco";
 
         Automovil mazda = new Automovil();
         mazda.fabricante = "Mazda";
@@ -14,14 +13,7 @@ public class EjemploAutomovil {
         mazda.cilindrada = 3.0;
         mazda.color = "Rojo";
 
-        System.out.println("auto.fabricante = " + auto.fabricante);
-        System.out.println("auto.modelo = " + auto.modelo);
-        System.out.println("auto.color = " + auto.color);
-        System.out.println("auto.cilindrada = " + auto.cilindrada);
-
-        System.out.println("mazda.fabricante = " + mazda.fabricante);
-        System.out.println("mazda.modelo = " + mazda.modelo);
-        System.out.println("mazda.color = " + mazda.color);
-        System.out.println("mazda.cilindrada = " + mazda.cilindrada);
+        System.out.println(subaru.detalle() + "\n");
+        System.out.println(mazda.detalle());
     }
 }
