12c363c83772641925d0afbc8e23ac7f142df276
Eraldo Vicente <eraldovicente2@gmail.com>
2021-09-13 13:10:04 -0300

    Utizando enum con sentencias switch case

---
+++ poo/src/EjemploAutomovilEnum.java
@@ -0,0 +1,30 @@
+public class EjemploAutomovilEnum {
+    public static void main(String[] args) {
+
+        Automovil.setCapacidadEstanqueEstatico(45);
+        Automovil subaru = new Automovil("Subaru", "Impreza");
+        subaru.setCilindrada(2.0);
+        subaru.setColor(Color.BLANCO);
+        subaru.setTipo(TipoAutomovil.HATCHBACK);
+
+        Automovil mazda = new Automovil("Mazda", "BT-50", Color.ROJO, 3.0);
+        mazda.setTipo(TipoAutomovil.PICKUP);
+        System.out.println("mazda.leerFabricante() = " + mazda.getFabricante());
+
+        TipoAutomovil tipo = subaru.getTipo();
+        System.out.println("Tipo subaru: " + tipo.getNombre());
+        System.out.println("Tipo desc. subaru: " + tipo.getDescripcion());
+
+        tipo = mazda.getTipo();
+
+        switch (tipo) {
+            case CONVERTIBLE -> System.out.println("El automovil es deportivo y descapotable de dos puertas");
+            case COUPE -> System.out.println("Es un automovil pequeño de dos puertas y tipicamente deportivo");
+            case FURGON -> System.out.println("Es un automovil utilitario de transporte, de empresas");
+            case HATCHBACK -> System.out.println("Es un automovil mediano compacto, aspecto deportivo");
+            case PICKUP -> System.out.println("Es un automovil de doble cabina o camioneta");
+            case SEDAN -> System.out.println("Es un automovil mediano");
+            case STATION_WAGON -> System.out.println("Es un automovil más grande, con maleta grande...");
+        }
+    }
+}
