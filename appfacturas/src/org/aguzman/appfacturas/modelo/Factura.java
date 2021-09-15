a7ae1d186c544878758be8ed6ff4fd91eb74daa5
Eraldo Vicente <eraldovicente2@gmail.com>
2021-09-15 17:06:31 -0300

    Creando la clase principal de aplicacion de facturas

--- appfacturas/src/org/aguzman/appfacturas/modelo/Factura.java
+++ appfacturas/src/org/aguzman/appfacturas/modelo/Factura.java
@@ -80,13 +80,13 @@ public class Factura {
                 .append(cliente.getNif())
                 .append("\nDescripción: ")
                 .append(this.descripcion)
-                .append("\n")
-                .append("\n#\tNombre\t$\tCant.\tTotal\n");
+                .append("\n");
 
         SimpleDateFormat df = new SimpleDateFormat("dd 'de' MMMM, yyyy");
         sb.append("Fecha Emisión: ")
                 .append(df.format(this.fecha))
-                .append("\n");
+                .append("\n")
+                .append("\n#\tNombre\t$\tCant.\tTotal\n");
 
         for (ItemFactura item : this.items) {
             if (item == null) {
