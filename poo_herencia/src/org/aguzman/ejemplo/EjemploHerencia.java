b6e9d48ffa151b6b17fb7d6f404a69dbf737ba50
Eraldo Vicente <eraldovicente2@gmail.com>
2021-09-16 00:30:19 -0300

    Herencia y el API de reflexion de Java

--- poo_herencia/src/org/aguzman/ejemplo/EjemploHerencia.java
+++ poo_herencia/src/org/aguzman/ejemplo/EjemploHerencia.java
@@ -7,15 +7,45 @@ public class EjemploHerencia {
         Alumno alumno = new Alumno();
         alumno.setNombre("Eraldo");
         alumno.setApellido("Vicente");
+        alumno.setInstitucion("Udemy");
+        alumno.setNotaCastellano(0.1);
+        alumno.setNotaHistoria(0.2);
+        alumno.setNotaMatematica(0.3);
+
+        AlumnoInternacional alumnoInt = new AlumnoInternacional();
+        alumnoInt.setNombre("Anna");
+        alumnoInt.setApellido("Vanina");
+        alumnoInt.setPais("Russia - Samara");
+        alumnoInt.setEdad(28);
+        alumnoInt.setInstitucion("...");
+        alumnoInt.setNotaIdiomas(10);
+        alumnoInt.setNotaCastellano(0.0);
+        alumnoInt.setNotaHistoria(10);
+        alumnoInt.setNotaMatematica(10);
 
         Profesor profesor = new Profesor();
         profesor.setNombre("Andres");
         profesor.setApellido("Guzman");
         profesor.setAsignatura("Desenvolvimento de Software");
 
-        System.out.println(alumno.getNombre() + " " + alumno.getApellido());
+        System.out.println(alumno.getNombre()
+                + " " + alumno.getApellido());
+
+        System.out.println(alumnoInt.getNombre()
+                + " " + alumnoInt.getApellido()
+                + " " + alumnoInt.getInstitucion()
+                + " " + alumnoInt.getPais());
+
         System.out.println("Profesor " + profesor.getAsignatura()
                 + ": " + profesor.getNombre()
                 + " " + profesor.getApellido());
+
+        Class clase = alumnoInt.getClass();
+        while (clase.getSuperclass() != null) {
+            String hija = clase.getName();
+            String padre = clase.getSuperclass().getName();
+            System.out.println(hija + " es una clase hija de la clase padre " + padre);
+            clase = clase.getSuperclass();
+        }
     }
 }
