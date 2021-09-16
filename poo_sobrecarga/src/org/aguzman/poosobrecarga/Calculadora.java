ba2c3f485b0b7f2281a3f02c965119d69b30e318
Eraldo Vicente <eraldovicente2@gmail.com>
2021-09-15 21:54:33 -0300

    Sobrecarga y metodos estaticos

--- poo_sobrecarga/src/org/aguzman/poosobrecarga/Calculadora.java
+++ poo_sobrecarga/src/org/aguzman/poosobrecarga/Calculadora.java
@@ -2,11 +2,14 @@ package org.aguzman.poosobrecarga;
 
 public class Calculadora {
 
-    public int sumar(int a, int b) {
+    private Calculadora() {
+    }
+
+    public static int sumar(int a, int b) {
         return a + b;
     }
 
-    public int sumar(int... argumentos) {
+    public static int sumar(int... argumentos) {
         int total = 0;
         for (int i: argumentos) {
             total+=i;
@@ -14,7 +17,7 @@ public class Calculadora {
         return total;
     }
 
-    public float sumar(float a, int... argumentos) {
+    public static float sumar(float a, int... argumentos) {
         float total = a;
         for (int i: argumentos) {
             total+=i;
@@ -22,7 +25,7 @@ public class Calculadora {
         return total;
     }
 
-    public double sumar(double... varargs) {
+    public static double sumar(double... varargs) {
         double total = 0.0;
         for (double d : varargs) {
             total += d;
@@ -30,23 +33,23 @@ public class Calculadora {
         return total;
     }
 
-    public float sumar(float x, float y) {
+    public static float sumar(float x, float y) {
         return x + y;
     }
 
-    public float sumar(int i, float j) {
+    public static float sumar(int i, float j) {
         return i + j;
     }
 
-    public float sumar(float i, int j) {
+    public static float sumar(float i, int j) {
         return i + j;
     }
 
-    public double sumar(double a, double b) {
+    public static double sumar(double a, double b) {
         return a + b;
     }
 
-    public int sumar(String a, String b) {
+    public static int sumar(String a, String b) {
         int resultado;
         try {
             resultado = Integer.parseInt(a) + Integer.parseInt(b);
@@ -56,7 +59,7 @@ public class Calculadora {
         return resultado;
     }
 
-    public int sumar(int a, int b, int c) {
+    public static int sumar(int a, int b, int c) {
         return a + b + c;
     }
 }
--- poo_sobrecarga/src/org/aguzman/poosobrecarga/EjemploSobrecarga.java
+++ poo_sobrecarga/src/org/aguzman/poosobrecarga/EjemploSobrecarga.java
@@ -1,24 +1,23 @@
 package org.aguzman.poosobrecarga;
+import static org.aguzman.poosobrecarga.Calculadora.*;
 
 public class EjemploSobrecarga {
     public static void main(String[] args) {
 
-        Calculadora cal = new Calculadora();
+        System.out.println("Sumar int: " + sumar(10, 5));
+        System.out.println("Sumar float: " + sumar(10.0F, 5F));
+        System.out.println("Sumar float-int: " + sumar(10F, 5));
+        System.out.println("Sumar int-float: " + sumar(10, 5.0F));
+        System.out.println("Sumar double: " + sumar(10, 5));
+        System.out.println("Sumar string: " + sumar("10", "5"));
+        System.out.println("Sumar tres int: " + sumar(10, 5, 3));
+        System.out.println("Sumar 4 int: " + sumar(10, 5, 3, 4));
+        System.out.println("Sumar 5 int: " + sumar(10, 5, 3, 4, 5));
+        System.out.println("Sumar float + n int: " + sumar(10.5F, 5, 9, 15));
+        System.out.println("Sumar 4 double: " + sumar(10.0, 5.0, 3.5, 4.5));
 
-        System.out.println("Sumar int: " + cal.sumar(10, 5));
-        System.out.println("Sumar float: " + cal.sumar(10.0F, 5F));
-        System.out.println("Sumar float-int: " + cal.sumar(10F, 5));
-        System.out.println("Sumar int-float: " + cal.sumar(10, 5.0F));
-        System.out.println("Sumar double: " + cal.sumar(10, 5));
-        System.out.println("Sumar string: " + cal.sumar("10", "5"));
-        System.out.println("Sumar tres int: " + cal.sumar(10, 5, 3));
-        System.out.println("Sumar 4 int: " + cal.sumar(10, 5, 3, 4));
-        System.out.println("Sumar 5 int: " + cal.sumar(10, 5, 3, 4, 5));
-        System.out.println("Sumar float + n int: " + cal.sumar(10.5F, 5, 9, 15));
-        System.out.println("Sumar 4 double: " + cal.sumar(10.0, 5.0, 3.5, 4.5));
-
-        System.out.println("Sumar long: " + cal.sumar(10L, 5L));
-        System.out.println("Sumar int: " + cal.sumar(10, '@'));
-        System.out.println("Sumar float-int: " + cal.sumar(10F, '@'));
+        System.out.println("Sumar long: " + sumar(10L, 5L));
+        System.out.println("Sumar int: " + sumar(10, '@'));
+        System.out.println("Sumar float-int: " + sumar(10F, '@'));
     }
 }
