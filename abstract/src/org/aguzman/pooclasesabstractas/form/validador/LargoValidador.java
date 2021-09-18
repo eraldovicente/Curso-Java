8837de33ee943d5de277dd0e1e8853526da3aafe
Eraldo Vicente <eraldovicente2@gmail.com>
2021-09-18 16:56:37 -0300

    Creando la clase concreta LargoValidador para validar el min y máx de caracteres

---
+++ abstract/src/org/aguzman/pooclasesabstractas/form/validador/LargoValidador.java
@@ -0,0 +1,44 @@
+package org.aguzman.pooclasesabstractas.form.validador;
+
+public class LargoValidador extends Validador {
+
+    protected String mensaje = "el campo debe tener mínimo %d caracteres y máximo %d caracteres";
+    private int min;
+    private int max = Integer.MAX_VALUE;
+
+    public LargoValidador() {
+    }
+
+    public LargoValidador(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    @Override
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    @Override
+    public boolean esValido(String valor) {
+        this.mensaje = String.format(this.mensaje, this.min, this.max);
+        if (valor == null) {
+            return true;
+        }
+        int largo = valor.length();
+        return (largo >= min && largo <= max);
+    }
+}
