ea3604e6bfcd7a7fd175503b4e71235114f8ade5
Eraldo Vicente <eraldovicente2@gmail.com>
2021-09-18 16:39:07 -0300

    Creando la clase abstracta Validador con tres clases hijas concretas

---
+++ abstract/src/org/aguzman/pooclasesabstractas/form/validador/NoNullValidador.java
@@ -0,0 +1,21 @@
+package org.aguzman.pooclasesabstractas.form.validador;
+
+public class NoNullValidador extends Validador{
+
+    protected String mensaje = "el campo no puede ser nulo";
+
+    @Override
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    @Override
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    @Override
+    public boolean esValido(String valor) {
+        return (valor != null);
+    }
+}
