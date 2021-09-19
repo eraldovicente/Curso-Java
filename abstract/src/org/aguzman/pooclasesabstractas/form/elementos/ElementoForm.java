1b48a36d5b01ec1467a7fb10b2556151e951e3ba
Eraldo Vicente <eraldovicente2@gmail.com>
2021-09-18 21:39:17 -0300

    Implementando la validacion en la clase abstracta ElementoForm

--- abstract/src/org/aguzman/pooclasesabstractas/form/elementos/ElementoForm.java
+++ abstract/src/org/aguzman/pooclasesabstractas/form/elementos/ElementoForm.java
@@ -1,11 +1,21 @@
 package org.aguzman.pooclasesabstractas.form.elementos;
 
+import org.aguzman.pooclasesabstractas.form.validador.Validador;
+
+import java.util.ArrayList;
+import java.util.List;
+
 abstract public class ElementoForm {
 
     protected String valor;
     protected String nombre;
 
+    private List<Validador> validadores;
+    private List<String> errores;
+
     public ElementoForm() {
+        this.validadores = new ArrayList<>();
+        this.errores = new ArrayList<>();
     }
 
     public ElementoForm(String nombre) {
@@ -13,8 +23,26 @@ abstract public class ElementoForm {
         this.nombre = nombre;
     }
 
+    public ElementoForm addValidador(Validador validador) {
+        this.validadores.add(validador);
+        return this;
+    }
+
+    public List<String> getErrores() {
+        return errores;
+    }
+
     public void setValor(String valor) {
         this.valor = valor;
+    }
+
+    public boolean esValido() {
+        for (Validador v: validadores) {
+            if (!v.esValido(this.valor)) {
+                this.errores.add(v.getMensaje());
+            }
+        }
+        return this.errores.isEmpty();
     }
 
     abstract public String dibujarHtml();
