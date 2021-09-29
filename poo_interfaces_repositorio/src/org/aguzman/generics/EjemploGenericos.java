79dc3af523d850502793a215fdadd36b699207af
Eraldo Vicente <eraldovicente2@gmail.com>
2021-09-29 19:49:05 -0300

    Metodos genericos maximo de tres objetos usando Comparable

--- poo_interfaces_repositorio/src/org/aguzman/generics/EjemploGenericos.java
+++ poo_interfaces_repositorio/src/org/aguzman/generics/EjemploGenericos.java
@@ -36,6 +36,11 @@ public class EjemploGenericos {
         imprimirClientes(clientesLista);
         imprimirClientes(clientesPrimiumList);
 
+        System.out.println("Máximo de 1, 9 y 4 es: " + maximo(1, 9, 4));
+        System.out.println("Máximo de 3.9, 11.6 y 7.78: " + maximo(3.9, 11.6, 7.78));
+        System.out.println("Máximo de Fabiana, 'te amo', 'tanto!': "
+                + maximo("Fabiana", " te amo ", " tanto!"));
+
     }
 
     public static <T> List<T> fromArrayToList(T[] c) {
@@ -59,5 +64,16 @@ public class EjemploGenericos {
 
     public static void imprimirClientes(List<? extends Cliente> clientes) {
         clientes.forEach(System.out::println);
+    }
+
+    public static <T extends Comparable<T>> T maximo(T a, T b, T c) {
+        T max = a;
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
+        return max;
     }
 }
