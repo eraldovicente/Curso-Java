4c0173b4cc2b041fc5f76f575dda282de8b5bc7e
Eraldo Vicente <eraldovicente2@gmail.com>
2021-10-01 18:50:59 -0300

    Formas de loop o iteraciones en los Collection Set y List

--- colecciones/src/org/aguzman/ejemplos/set/EjemploHashSetUnicidad.java
+++ colecciones/src/org/aguzman/ejemplos/set/EjemploHashSetUnicidad.java
@@ -2,13 +2,13 @@ package org.aguzman.ejemplos.set;
 
 import org.aguzman.ejemplos.modelo.Alumno;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class EjemploHashSetUnicidad {
     public static void main(String[] args) {
 
-        Set<Alumno> sa = new HashSet<>();
+        //Set<Alumno> sa = new HashSet<>();
+        List<Alumno> sa = new ArrayList<>();
         sa.add(new Alumno("Pato", 5));
         sa.add(new Alumno("Cata", 6));
         sa.add(new Alumno("Luci", 4));
@@ -16,7 +16,26 @@ public class EjemploHashSetUnicidad {
         sa.add(new Alumno("Andres", 3));
         sa.add(new Alumno("Zeus", 2));
         sa.add(new Alumno("Zeus", 2));
+        //System.out.println(sa);
 
-        System.out.println(sa);
+        System.out.println("Utilizando un for clásico");
+        for (int i = 0; i < sa.size(); i++) {
+            Alumno a = sa.get(i);
+            System.out.println(a.getNombre());
+        }
+        System.out.println("Iterando usando un foreach");
+        for (Alumno a: sa) {
+            System.out.println(a.getNombre());
+        }
+
+        System.out.println("Iterando usando while y iterator");
+        Iterator<Alumno> it = sa.iterator();
+        while (it.hasNext()) {
+            Alumno a = it.next();;
+            System.out.println(a.getNombre());
+        }
+
+        System.out.println("Iterando usando Stream forEach");
+        sa.forEach(System.out::println);
     }
 }
