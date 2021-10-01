1b37f21798ac0929979e34d568ea2631003e1a42
Eraldo Vicente <eraldovicente2@gmail.com>
2021-10-01 16:32:15 -0300

    Singularidad en TreeSet y HashSet

--- colecciones/src/org/aguzman/ejemplos/modelo/Alumno.java
+++ colecciones/src/org/aguzman/ejemplos/modelo/Alumno.java
@@ -1,5 +1,7 @@
 package org.aguzman.ejemplos.modelo;
 
+import java.util.Objects;
+
 public class Alumno implements Comparable<Alumno>{
     private String nombre;
     private Integer nota;
@@ -51,5 +53,18 @@ public class Alumno implements Comparable<Alumno>{
             return 0;
         }
         return this.nota.compareTo(a.nota);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Alumno alumno = (Alumno) o;
+        return Objects.equals(nombre, alumno.nombre) && Objects.equals(nota, alumno.nota);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, nota);
     }
 }
---
+++ colecciones/src/org/aguzman/ejemplos/set/EjemploHashSetUnicidad.java
@@ -0,0 +1,22 @@
+package org.aguzman.ejemplos.set;
+
+import org.aguzman.ejemplos.modelo.Alumno;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class EjemploHashSetUnicidad {
+    public static void main(String[] args) {
+
+        Set<Alumno> sa = new HashSet<>();
+        sa.add(new Alumno("Pato", 5));
+        sa.add(new Alumno("Cata", 6));
+        sa.add(new Alumno("Luci", 4));
+        sa.add(new Alumno("Jano", 7));
+        sa.add(new Alumno("Andres", 3));
+        sa.add(new Alumno("Zeus", 2));
+        sa.add(new Alumno("Zeus", 2));
+
+        System.out.println(sa);
+    }
+}
