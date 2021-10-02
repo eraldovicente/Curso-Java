bb9ddd869354c91e1043fd9b624f437b1845e06b
Eraldo Vicente <eraldovicente2@gmail.com>
2021-10-01 21:30:26 -0300

    Ejemplo LinkedList (Lista doblemente enlazada) - Pilas y colas

---
+++ colecciones/src/org/aguzman/ejemplos/set/EjemploLinkedList.java
@@ -0,0 +1,52 @@
+package org.aguzman.ejemplos.set;
+
+import org.aguzman.ejemplos.modelo.Alumno;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class EjemploLinkedList {
+    public static void main(String[] args) {
+
+        LinkedList<Alumno> enlazada = new LinkedList<>();
+        System.out.println(enlazada + ", size = " + enlazada.size());
+        System.out.println("Está vacia = " + enlazada.isEmpty());
+        enlazada.add(new Alumno("Pato", 5));
+        enlazada.add(new Alumno("Cata", 6));
+        enlazada.add(new Alumno("Luci", 4));
+        enlazada.add(new Alumno("Jano", 7));
+        enlazada.add(new Alumno("Andres", 3));
+
+        System.out.println(enlazada + ", size = " + enlazada.size());
+
+        enlazada.addFirst(new Alumno("Zeus", 5));
+        enlazada.addLast(new Alumno("Atenea", 6));
+
+        System.out.println(enlazada + ", size = " + enlazada.size());
+
+        System.out.println("Primero = " + enlazada.getFirst());
+        System.out.println("Primero = " + enlazada.peekFirst());
+        System.out.println("Último = " + enlazada.getLast());
+        System.out.println("Último = " + enlazada.peekLast());
+        System.out.println("Indice 2 = " + enlazada.get(2));
+
+        //Alumno zeus = enlazada.removeFirst();
+        //Alumno zeusPoll = enlazada.pollFirst();
+        Alumno zeusPop = enlazada.pop();
+        enlazada.removeLast();
+        enlazada.pollLast();
+
+        System.out.println(enlazada + ", size = " + enlazada.size());
+
+        Alumno a = new Alumno("Lucas", 8);
+        enlazada.addLast(a);
+        System.out.println("Indice de Lucas = " + enlazada.indexOf(a));
+
+        enlazada.remove(2);
+        System.out.println(enlazada + ", size = " + enlazada.size());
+
+        enlazada.set(3, new Alumno("Lalo", 7));
+        System.out.println(enlazada + ", size = " + enlazada.size());
+    }
+}
