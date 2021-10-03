81311e43a431cde6d42d55f3626a47532c3c08a5
Eraldo Vicente <eraldovicente2@gmail.com>
2021-10-02 23:17:27 -0300

    El metodo join y sleep

--- hilos/src/org/aguzman/hilos/ejemplos/EjemploInterfaceRunnableJava8.java
+++ hilos/src/org/aguzman/hilos/ejemplos/EjemploInterfaceRunnableJava8.java
@@ -3,8 +3,9 @@ package org.aguzman.hilos.ejemplos;
 import org.aguzman.hilos.ejemplos.runnable.ViajeTarea;
 
 public class EjemploInterfaceRunnableJava8 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        Thread main = Thread.currentThread();
         Runnable viaje = () -> {
             for (int i = 0; i < 10; i++) {
                 System.out.println(i + " - " + Thread.currentThread().getName());
@@ -15,11 +16,25 @@ public class EjemploInterfaceRunnableJava8 {
                 }
             }
             System.out.println("Finalmente me voy de viaje a: " + Thread.currentThread().getName());
+            System.out.println(main.getState());
         };
 
-        new Thread(viaje, "Isla de Pascua").start();
-        new Thread(viaje, "Robison Crusoe").start();
-        new Thread(viaje, "Juan Fernandes").start();
-        new Thread(viaje, "Isla de Chiloe").start();
+        Thread v1 = new Thread(viaje, "Isla de Pascua");
+        Thread v2 = new Thread(viaje, "Robison Crusoe");
+        Thread v3 = new Thread(viaje, "Juan Fernandes");
+        Thread v4 = new Thread(viaje, "Isla de Chiloe");
+
+        v1.start();
+        v2.start();
+        v3.start();
+        v4.start();
+        v1.join();
+        v2.join();
+        v3.join();
+        v4.join();
+
+        //Thread.sleep(1000);
+
+        System.out.println("Continuando con la ejecución del método main: " + main.getName());
     }
 }
