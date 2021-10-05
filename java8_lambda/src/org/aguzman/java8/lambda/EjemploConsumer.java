bddcc13839c1970547f63226e695ce681815df96
Eraldo Vicente <eraldovicente2@gmail.com>
2021-10-04 21:04:13 -0300

    Referenciasde metodos en expresiones lambdade de tipo consumer

--- java8_lambda/src/org/aguzman/java8/lambda/EjemploConsumer.java
+++ java8_lambda/src/org/aguzman/java8/lambda/EjemploConsumer.java
@@ -1,9 +1,14 @@
 package org.aguzman.java8.lambda;
 
+import org.aguzman.java8.lambda.models.Usuario;
+
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class EjemploConsumer {
     public static void main(String[] args) {
@@ -14,10 +19,25 @@ public class EjemploConsumer {
         };
         consumidor.accept(new Date());
 
-        BiConsumer<String, Integer> consumidorBi = (nombre, edad) -> {
-            System.out.println(nombre + ", tiene" + edad + " años");
-        };
+        BiConsumer<String, Integer> consumidorBi = (nombre, edad) -> System.out.println(nombre + ", tiene" + edad + " años");
 
         consumidorBi.accept("pepe", 20);
+
+        Consumer<String> consumidor2 = System.out::println;
+        consumidor2.accept("Hola mundo lambda");
+
+        List<String> nombres = Arrays.asList("andres", "pepe", "luz", "paco");
+        nombres.forEach(consumidor2);
+
+        Supplier<Usuario> creaUsuario = Usuario::new;
+        Usuario usuario = creaUsuario.get();
+
+        BiConsumer<Usuario, String> asignarNombre = Usuario::setNombre;
+        asignarNombre.accept(usuario, "Andrés");
+        System.out.println("Nombres usuario: " + usuario.getNombre());
+
+        Supplier<String> proveedor = () -> "Hola mundo lambda supplier";
+
+        System.out.println(proveedor.get());
     }
 }
