b72ff68939653fa30017966edf6529c9dc0664c0
Eraldo Vicente <eraldovicente2@gmail.com>
2021-10-14 16:16:20 -0300

    Un resumen al API Optinal de Java

--- api_stream/src/org/aguzman/api/stream/ejemplos/EjemploStreamFilterSingle.java
+++ api_stream/src/org/aguzman/api/stream/ejemplos/EjemploStreamFilterSingle.java
@@ -18,7 +18,13 @@ public class EjemploStreamFilterSingle {
                 .peek(System.out::println);
 
         Optional<Usuario> usuario = nombres.findFirst();
-        System.out.println(usuario.get());
+        //System.out.println(usuario.orElse(new Usuario("John", "Doe")).getNombre());
+        //System.out.println(usuario.orElseGet(() -> new Usuario("John", "Doe")).getNombre());
+        if (usuario.isPresent()) {
+            System.out.println(usuario.get().getNombre());
+        } else {
+            System.out.println("No se encontró el objeto");
+        }
         //nombres.forEach(System.out::println);
     }
 }
